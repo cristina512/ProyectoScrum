85ff182051d37478ffd5dfa2ef83340e412dc7b1
Francisco Grande <grandefrancisco43@gmail.com>
2025-04-01 19:32:03 +0200

    cambio2

--- base-datos/proyecto.sql
+++ base-datos/proyecto.sql
@@ -9,3 +9,15 @@ CREATE TABLE USUARIO (
     gestor_id INT,
     CONSTRAINT fk_gestor FOREIGN KEY (gestor_id) REFERENCES GESTOR(id)
 );
+
+
+CREATE TABLE Equipo (
+    id_equipo NUMBER PRIMARY KEY,
+    nombre VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE Tarea (
+    id_tarea NUMBER PRIMARY KEY,
+    descripcion VARCHAR2(255) NOT NULL,
+    fecha_limite DATE
+);
