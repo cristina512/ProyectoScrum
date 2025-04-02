a44a4987df35cc008cddf311ed2e44cad1347130
diegogarciia <diegogarciaangel25@gmail.com>
2025-04-02 12:02:26 +0200

    cambio4

--- base-datos/proyecto.sql
+++ base-datos/proyecto.sql
@@ -1,42 +1,44 @@
 CREATE TABLE GESTOR (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE USUARIO (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(100) NOT NULL,
     gestor_id INT,
-    CONSTRAINT fk_gestor FOREIGN KEY (gestor_id) REFERENCES GESTOR(id)
+    CONSTRAINT fk_gestor FOREIGN KEY (gestor_id) REFERENCES GESTOR(id)
 );
 
 CREATE TABLE Equipo (
-    id_equipo NUMBER PRIMARY KEY,
-    nombre VARCHAR2(100) NOT NULL
+    id_equipo INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Tarea (
-    id_tarea NUMBER PRIMARY KEY,
-    descripcion VARCHAR2(255) NOT NULL,
+    id_tarea INT PRIMARY KEY AUTO_INCREMENT,
+    descripcion VARCHAR(255) NOT NULL,
     fecha_limite DATE,
-    FOREIGN KEY (usuario_creador_id) REFERENCES Usuario(id),
+    usuario_creador_id INT NOT NULL,
+    equipo_id INT NOT NULL,
+    FOREIGN KEY (usuario_creador_id) REFERENCES USUARIO(id),
     FOREIGN KEY (equipo_id) REFERENCES Equipo(id_equipo)
 );
 
 CREATE TABLE Usuario_Equipo (
-    usuario_id NUMBER PRIMARY KEY,
-    equipo_id NUMBER PRIMARY KEY,
+    usuario_id INT,
+    equipo_id INT,
     PRIMARY KEY (usuario_id, equipo_id),
-    FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+    FOREIGN KEY (usuario_id) REFERENCES USUARIO(id),
     FOREIGN KEY (equipo_id) REFERENCES Equipo(id_equipo)
 );
 
 CREATE TABLE Asignacion_Tarea (
-    id_asignacion NUMBER PRIMARY KEY,
-    tarea_id NUMBER NOT NULL,
-    usuario_asigna_id NUMBER NOT NULL,
-    usuario_recibe_id NUMBER NOT NULL,
+    id_asignacion INT PRIMARY KEY AUTO_INCREMENT,
+    tarea_id INT NOT NULL,
+    usuario_asigna_id INT NOT NULL,
+    usuario_recibe_id INT NOT NULL,
     FOREIGN KEY (tarea_id) REFERENCES Tarea(id_tarea),
-    FOREIGN KEY (usuario_asigna_id) REFERENCES Usuario(id),
-    FOREIGN KEY (usuario_recibe_id) REFERENCES Usuario(id)
+    FOREIGN KEY (usuario_asigna_id) REFERENCES USUARIO(id),
+    FOREIGN KEY (usuario_recibe_id) REFERENCES USUARIO(id)
 );
